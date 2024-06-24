32afa29309abd106063902b44f09bbe1fe361774
bushraasabir <bushrasabir508@gmail.com>
2024-06-24 17:31:15 +0500

    Push my game to github

---
+++ index.js
@@ -0,0 +1,17 @@
+#! urs/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 8 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-8:",
+    },
+]);
+console.log(answer);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! you have guessed right number.");
+}
+else {
+    console.log("You have guessed wrong number");
+}
